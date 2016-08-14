c2139f17995fe15cbcbf507a242179eb2795c7b0
Jose Alanya <jose.alanya@gmail.com>
2016-08-13 23:53:20 -0500

    Render Books through a router with their views

--- app.js
+++ app.js
@@ -4,17 +4,41 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
+var nav = [{
+        Link: '/Books',
+        Text: 'Book'
+    },
+      {
+        Link: '/Authors',
+        Text: 'Author'
+    }];
+
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use('/Books', bookRouter);
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
-  });
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+                Link: '/Books',
+                Text: 'Books'
+            },
+              {
+                Link: '/Authors',
+                Text: 'Authors'
+            }]
+    });
+});
 
 app.get('/books', function (req, res) {
     res.send('Hello Books');
-  });
+});
 
 app.listen(port, function () {
     console.log('Running server on port ' + port);
-  });
+});
