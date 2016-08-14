176759b561ed853ce9bc24907068e311fe5cd0a9
Jose Alanya <jose.alanya@gmail.com>
2016-08-13 20:00:26 -0500

    Working with Nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
@@ -27,10 +28,27 @@ gulp.task('inject', function() {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public'
-    }
+    };
 
     return gulp.src('./src/views/*.html')
           .pipe(wiredep(options))
           .pipe(inject(injectSrc, injectOptions))
           .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function() {
+    var options = {
+        scripts: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function(ev) {
+          console.log('Restarting...');
+        });
+
 });
